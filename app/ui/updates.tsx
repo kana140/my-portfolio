0c68be80eac1f435a803ef0f6c4729c6974d6db3
Keitel Anana <88295540+kana140@users.noreply.github.com>
2026-02-28 15:20:49 +1300

    add update text file app and icon

---
+++ app/ui/updates.tsx
@@ -0,0 +1,8 @@
+export default function Updates() {
+  return (
+    <div className="bg-white">
+      <p>28/02/2026</p>
+      <li> allow opening multiple windows at once </li>
+    </div>
+  );
+}
